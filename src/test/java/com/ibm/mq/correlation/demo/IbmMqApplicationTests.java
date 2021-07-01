15567ef5106bc00c2cb1d949c809041d1ec9fce9
Denis Kuznecov <dennis.kuznecov.1999@gmail.com>
2021-07-01 21:06:24 +0400

    init

---
+++ src/test/java/com/ibm/mq/correlation/demo/IbmMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.mq.correlation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
