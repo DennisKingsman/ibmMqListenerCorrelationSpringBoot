556881d56283f5638a3a39638b14df8e7a29f704
Denis Kuznecov <dennis.kuznecov.1999@gmail.com>
2021-07-02 15:52:29 +0400

    request to mq

--- src/main/java/com/ibm/mq/correlation/demo/IbmMqApplication.java
+++ src/main/java/com/ibm/mq/correlation/demo/IbmMqApplication.java
@@ -2,8 +2,10 @@ package com.ibm.mq.correlation.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class IbmMqApplication {
 
 	public static void main(String[] args) {
